ea78c7cdcf822106926dc4e4f361dafe1606c9d3
ArthurSven <arthursvenmsiska@gmail.com>
2023-12-26 09:57:04 +0200

    created learning columns

--- app/src/main/java/com/devapps/justspeak_10/ui/Screens/German/GermanRouterHome.kt
+++ app/src/main/java/com/devapps/justspeak_10/ui/Screens/German/GermanRouterHome.kt
@@ -2,18 +2,27 @@ package com.devapps.justspeak_10.ui.Screens.German
 
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,9 +38,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,6 +53,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.devapps.justspeak_10.R
 import com.devapps.justspeak_10.data.remote.model.UserData
 import com.devapps.justspeak_10.data.remote.repository.GoogleClientAuth
 import com.devapps.justspeak_10.ui.Components.UserBar
@@ -74,7 +86,7 @@ fun GermanNavigation(startNavController: NavController) {
         composable(GermanHomeScreen.route) {
             GermanHomeContent(
                 userData = googleClientAuth.getSignedInUser(),
-                navController = germanNavController,
+                cardNavController = germanNavController,
                 onSignOut = {
                     coroutineScope.launch {
                         googleClientAuth.signOut()
@@ -109,7 +121,7 @@ fun GermanNavigation(startNavController: NavController) {
 @Composable
 fun GermanHomeContent(
     userData: UserData?,
-    navController: NavController,
+    cardNavController: NavController,
     onSignOut: () -> Unit
 ) {
         Surface(
@@ -144,7 +156,7 @@ fun GermanHomeContent(
                                            color = Color.Black)
                                 },
                                 onClick = {
-                                          navController.navigate(Signout.route)
+                                          cardNavController.navigate(Signout.route)
                                     onSignOut()
                                 },
                                 modifier = Modifier
@@ -166,6 +178,7 @@ fun GermanHomeContent(
                         .padding(it)
                         .background(color = Color.LightGray)
                 ) {
+
                     UserBar(userData)
                     Column(
                         modifier = Modifier
@@ -178,7 +191,16 @@ fun GermanHomeContent(
                         Text(text = "Let's started with...",
                             fontSize = 18.sp,
                             color = Color.Black,
-                            fontWeight = FontWeight.Bold)
+                            fontWeight = FontWeight.Bold
+                        )
+                        Spacer(modifier = Modifier
+                            .height(20.dp))
+                        LazyVerticalGrid(columns = GridCells.Fixed(2),
+                            content = {
+                                items(4) { i ->
+                                    val cardItem = items[i]
+                                }
+                            })
                     }
                 }
         }
@@ -187,7 +209,50 @@ fun GermanHomeContent(
 
     }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GermanCard(
+    onClick: () -> Unit,
+    icon: Int,
+    cardTitle: String
+) {
+    ElevatedCard(
+        onClick = {
+                  onClick()
+        },
+        colors = CardDefaults.cardColors( containerColor = Color.White),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        modifier = Modifier
+            .height(170.dp)
+            .width(150.dp)
+            .padding(all = 20.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(all = 10.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                )
+            Text(text = cardTitle,
+                color = Color.Black,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+                )
+        }
+    }
+}
+
 @Composable
 @Preview(showBackground = true)
 fun ShowGermanScreens() {
+    
 }
