367d8fbdc8aa5387eda1e9d763ecbfb4b02b54db
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2024-04-30 17:10:43 +0200

    Adding German time, almost done wth phrase section

---
+++ app/src/main/java/com/devapps/justspeak_10/data/local/model/Journal.kt
@@ -0,0 +1,47 @@
+package com.devapps.justspeak_10.data.local.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
+
+@Entity(tableName = "journal")
+data class Journal(
+    @PrimaryKey(autoGenerate = true)
+    val journalId: Int = 0,
+    @ColumnInfo("journal_name")
+    val journalName: String,
+    @ColumnInfo("date_created")
+    val date: String,
+    @ColumnInfo("äuthor")
+    val author: String
+)
+
+@Serializable
+data class JournalNetwork(
+    val journalName: String,
+    val date: String,
+    val author: String
+)
+
+@Entity(tableName = "journal_entry")
+data class JournalEntry(
+    @PrimaryKey(autoGenerate = true)
+    val journalEntryId: Int = 0,
+    @ColumnInfo("entry")
+    val entry: String,
+    @ColumnInfo("date")
+    val date: String,
+    @ColumnInfo("author")
+    val author: String,
+    @ColumnInfo("journal_id")
+    val journalId: Int
+)
+
+@Serializable
+data class JournalEntryNetwork(
+    val entry: String,
+    val date: String,
+    val author: String,
+    val journalId: Int
+)
