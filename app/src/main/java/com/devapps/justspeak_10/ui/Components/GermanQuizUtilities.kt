42d1cb181ef902e6fe938caf201b854570ad0096
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2024-05-15 15:40:08 +0200

    Building case quiz

--- app/src/main/java/com/devapps/justspeak_10/ui/Components/GermanQuizUtilities.kt
+++ app/src/main/java/com/devapps/justspeak_10/ui/Components/GermanQuizUtilities.kt
@@ -12,11 +12,36 @@ fun germanCaseQuizQuestions() : List<Question> {
     return listOf(
         Question(
             "1",
-            "",
+            "Der, die das are examples of...",
             listOf(
-                "", "", ""
+                "Definite articles", "Indefinite Articles",
             ),
-            ""
-        )
+            "Definite Articles"
+        ),
+        Question(
+            "2",
+            "What case is the following sentence - Ich kaufe einen neuen Computer",
+            listOf(
+                "Nominativ", "Dativ", "Akkusativ", "Genitiv"
+            ),
+            "Akkusativ"
+        ),
+        Question(
+            "3",
+            "Complete the sentence - Das Gepäck ... Frau ist verloren (The woman's bag is lost)",
+            listOf(
+                "die", "des", "der"
+            ),
+            "der"
+        ),
+        Question(
+            "4",
+            "Ich gab ... ... Mann den Ball (I gave the tall man the ball.)",
+            listOf(
+                "dem große", "den großen", "der großem", "dem großen"
+            ),
+            "dem großen"
+        ),
+        Question()
     )
 }
